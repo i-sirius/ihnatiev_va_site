8b53faa68e63a28a9433943bc23684872c396617
i-sirius <pollux.twin@gmail.com>
2026-05-12 18:49:38 +0300

    Hotfix 0.6.18b mobile header typography

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
   meta: {
     siteTitle: "Віталій Ігнатьєв",
     ownerName: "Sirius",
-    buildVersion: "0.6.18a",
+    buildVersion: "0.6.18b",
     buildDate: "120526",
     year: "2026",
     homeTitle: "Ігнатьєв Віталій",
