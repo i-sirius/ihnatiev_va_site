33e376bcc15f0a277f2f2798099e446fb069845a
i-sirius <pollux.twin@gmail.com>
2026-05-12 18:36:52 +0300

    Release 0.6.18 responsive polish

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
   meta: {
     siteTitle: "Віталій Ігнатьєв",
     ownerName: "Sirius",
-    buildVersion: "0.6.17b",
+    buildVersion: "0.6.18",
     buildDate: "120526",
     year: "2026",
     homeTitle: "Ігнатьєв Віталій",
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "ihnatiev-site-v0.6.17b-r11";
+const CACHE_NAME = "ihnatiev-site-v0.6.18-r14";
 const APP_SHELL = [
   "./",
   "./index.html",
