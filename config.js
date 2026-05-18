3225296bdc9c79f4632b1826fda7c741b87ded84
i-sirius <pollux.twin@gmail.com>
2026-05-18 22:38:54 +0300

    v0.6.21b: tune typography spacing and readability

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
   meta: {
     siteTitle: "Віталій Ігнатьєв",
     ownerName: "Sirius",
-    buildVersion: "0.6.21a",
+    buildVersion: "0.6.21b",
     buildDate: "180526",
     year: "2026",
     homeTitle: "Ігнатьєв Віталій",
