b5ae9e17d5e710b1dfc3cbc2b0830cb0693de4a0
i-sirius <pollux.twin@gmail.com>
2026-05-20 02:44:40 +0300

    v0.6.23j: stabilize publication badges

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
   meta: {
     siteTitle: "Віталій Ігнатьєв",
     ownerName: "Sirius",
-    buildVersion: "0.6.23i",
+    buildVersion: "0.6.23j",
     buildDate: "180526",
     year: "2026",
     homeTitle: "Ігнатьєв Віталій",
