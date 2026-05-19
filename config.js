8154a219f82bfff5c64df84e5bf30eb1f1cd36d3
i-sirius <pollux.twin@gmail.com>
2026-05-19 11:44:39 +0300

    v0.6.21k: soften navigation active states

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
   meta: {
     siteTitle: "Віталій Ігнатьєв",
     ownerName: "Sirius",
-    buildVersion: "0.6.21j",
+    buildVersion: "0.6.21k",
     buildDate: "180526",
     year: "2026",
     homeTitle: "Ігнатьєв Віталій",
