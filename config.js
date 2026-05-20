3c8c23a76c1b5b9f6fc644a3558e88e4e631f750
i-sirius <pollux.twin@gmail.com>
2026-05-20 12:26:17 +0300

    v0.6.24f: segment details toggle icon

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
   meta: {
     siteTitle: "Віталій Ігнатьєв",
     ownerName: "Sirius",
-    buildVersion: "0.6.24e",
+    buildVersion: "0.6.24f",
     buildDate: "180526",
     year: "2026",
     homeTitle: "Ігнатьєв Віталій",
