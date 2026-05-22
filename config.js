e3808a550b2162a1456a53533872655434b425f7
i-sirius <pollux.twin@gmail.com>
2026-05-22 13:25:28 +0300

    v0.6.28h: align header navigation controls

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
   meta: {
     siteTitle: "Віталій Ігнатьєв",
     ownerName: "Sirius",
-    buildVersion: "0.6.28g",
+    buildVersion: "0.6.28h",
     buildDate: "210526",
     year: "2026",
     homeTitle: "Ігнатьєв Віталій",
