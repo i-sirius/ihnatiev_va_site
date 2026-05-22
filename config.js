180d45f6a45262a2085753316ef70956b9f775e1
i-sirius <pollux.twin@gmail.com>
2026-05-22 13:50:39 +0300

    v0.6.28k: add mobile header stack backings

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
   meta: {
     siteTitle: "Віталій Ігнатьєв",
     ownerName: "Sirius",
-    buildVersion: "0.6.28j",
+    buildVersion: "0.6.28k",
     buildDate: "210526",
     year: "2026",
     homeTitle: "Ігнатьєв Віталій",
