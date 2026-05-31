00f9a6e52e01b087c2dbf540ef3e26d5a3ce09ee
i-sirius <pollux.twin@gmail.com>
2026-05-31 12:09:55 +0300

    release: 0.7.77f.310526 - fix legacy encoding regression

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-﻿const CACHE_NAME = "ihnatiev-site-v0.7.77e.310526-r1";
+const CACHE_NAME = "ihnatiev-site-v0.7.77f.310526-r1";
 const APP_SHELL = [
   "./",
   "./index.html",
@@ -124,4 +124,3 @@ self.addEventListener("fetch", (event) => {
     })
   );
 });
-
