a45e8e8203b73d0c40bf1dd4b33bc139614e08ea
i-sirius <pollux.twin@gmail.com>
2026-06-11 10:37:20 +0300

    release: 0.7.77m.110626 - polish accessible details and socials

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ const SITE = {
     ownerName: "Sirius",
     authorRealName: "Віталій Ігнатьєв",
     siteDeveloperName: "Sirius",
-    buildVersion: "0.7.77l",
+    buildVersion: "0.7.77m",
     buildDate: "110626",
     year: "2026",
     homeTitle: "Ігнатьєв Віталій",
