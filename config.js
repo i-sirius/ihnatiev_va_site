f818f8c6f08259ea52fc9098f471bbd2c6fc6ab2
i-sirius <pollux.twin@gmail.com>
2026-06-11 11:20:57 +0300

    release: 0.7.77n.110626 - polish accessible activity UI

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ const SITE = {
     ownerName: "Sirius",
     authorRealName: "Віталій Ігнатьєв",
     siteDeveloperName: "Sirius",
-    buildVersion: "0.7.77m",
+    buildVersion: "0.7.77n",
     buildDate: "110626",
     year: "2026",
     homeTitle: "Ігнатьєв Віталій",
