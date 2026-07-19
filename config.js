5edab5f4896ed1662ad698521c242333702aed38
i-sirius <pollux.twin@gmail.com>
2026-07-19 19:36:46 +0300

    release: 0.7.80a.290626 - update sermons audio section

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ const SITE = {
     ownerName: "Sirius",
     authorRealName: "Віталій Ігнатьєв",
     siteDeveloperName: "Sirius",
-    buildVersion: "0.7.80",
+    buildVersion: "0.7.80a",
     buildDate: "290626",
     year: "2026",
     homeTitle: "Ігнатьєв Віталій",
